27f837567ef8bebd0af58f7374d29a30ae5cdcf5
yosif11 <49305870+yosif11@users.noreply.github.com>
2020-08-02 08:41:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+const express = require("express");
+const app = express();
+
+app.listen(() => console.log("start btrolie"));
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const cmd = require("node-cmd");
+const ms = require("ms");
+const fs = require('fs');
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const convert = require("hh-mm-ss")
+const fetchVideoInfo = require("youtube-info");
+const simpleytapi = require('simple-youtube-api')
+const util = require("util")
+const gif = require("gif-search");
+const jimp = require("jimp");
+const guild = require('guild');
+const hastebins = require('hastebin-gen');
+const getYoutubeID = require('get-youtube-id');
+const pretty = require("pretty-ms");
+const moment = require('moment');
+const request = require('request');
+const dateFormat = require('dateformat');
+
+//لا تلعب اي شي في الكود
+
+
+
+const prefix = "" //البرفكس ياحلو
+const developers = "" // الايدي 
+
+
+
 const sug = require("./suggestions.json")
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -65,3 +105,7 @@ client.on("message", msg => {
             }
         
 })
+
+
+
+client.login("");//التوكن
